ec21dc2fee287bc42169310edd523d0507984210
Hawari <naufalhawarden@gmail.com>
2023-11-26 22:13:00 +0700

    updating

--- client.py
+++ client.py
@@ -4,13 +4,16 @@ class ClinicClient:
     def __init__(self, server_address):
         self.server = xmlrpc.client.ServerProxy(server_address)
 
-    def register_patient(self, clinic_name, patient_info):
-        result = self.server.register_patient(clinic_name, patient_info)
+    def register_patient(self, clinic_id, patient_info):
+        result = self.server.register_patient(clinic_id, patient_info)
         print(result)
 
-    def get_queue_status(self, clinic_name):
-        status = self.server.get_queue_status(clinic_name)
-        print(f"Queue Status for {clinic_name}: {status}")
+    def get_queue_status(self, clinic_id):
+        status = self.server.get_queue_status(clinic_id)
+        print(f"Status antrian: {status}")
+
+    def get_menu(self):
+        print(self.server.get_menu())
 
 if __name__ == "__main__":
     server_address = "http://192.168.100.9:8000"  # Replace <server-ip> with the actual IP address of the server device
@@ -18,34 +21,19 @@ if __name__ == "__main__":
 
 
     
-    print("""Selamat Datang di Antredis
-Klinik tersedia:
-1. Klinik A
-2. Klinik B
-3. Klinik C
-0. Keluar Aplikasi""")
+    client.get_menu()
     
     pilihan_menu = int(input("Pilihan menu: "))
     while pilihan_menu <= 3 and pilihan_menu >= 1:
-        if pilihan_menu == 1:
-            clinic_name = "Klinik A"
-        if pilihan_menu == 2:
-            clinic_name = "Klinik B"
-        if pilihan_menu == 3:
-            clinic_name = "Klinik C"
 
         medic_record_number = int(input("Nomor rekam medis: "))
         name = input("Nama Lengkap: ")
         dob = input("Tanggal Lahir (contoh: 30-01-2003): ")
 
         patient_info = {"medic_record_number": medic_record_number, "name": name, "dob": dob}
-        client.register_patient(clinic_name, patient_info)
-        client.get_queue_status(clinic_name)
+        client.register_patient(pilihan_menu, patient_info)
+        client.get_queue_status(pilihan_menu)
 
+        pilihan_menu = int(input("Pilihan menu: "))
 
-        print("""Selamat Datang di Antredis
-Klinik tersedia:
-1. Klinik A
-2. Klinik B
-3. Klinik C
-0. Keluar Aplikasi""")
+        client.get_menu()
--- server.py
+++ server.py
@@ -1,35 +1,56 @@
 from xmlrpc.server import SimpleXMLRPCServer
+import threading
 import time
+
+QUEUE_TIME = 10
 
 class ClinicServer:
     def __init__(self):
         self.klinik_status = {
-            "Klinik A": {"current_queue": 0, "estimated_wait_time": 0},
-            "Klinik B": {"current_queue": 0, "estimated_wait_time": 0},
-            "Klinik C": {"current_queue": 0, "estimated_wait_time": 0},
+            1: {"name": "Klinik A", "current_queue": 0, "estimated_wait_time": 0, "open": True, "patients": []},
+            2: {"name": "Klinik B", "current_queue": 0, "estimated_wait_time": 0, "open": False, "patients": []},
+            3: {"name": "Klinik C", "current_queue": 0, "estimated_wait_time": 0, "open": True, "patients": []},
         }
 
-    def register_patient(self, clinic_name, patient_info):
+
+
+    def register_patient(self, clinic_id, patient_info):
+
+
         
-
         # For simplicity, we'll just increment the queue count
-        self.klinik_status[clinic_name]["current_queue"] += 1
-        self.klinik_status[clinic_name]["estimated_wait_time"] += 10  # 10 Minutes per patient
-        return f"Registration successful for {clinic_name}. Current Queue: {self.klinik_status[clinic_name]['current_queue']}"
+        self.klinik_status[clinic_id]["current_queue"] += 1
+        self.klinik_status[clinic_id]["estimated_wait_time"] += QUEUE_TIME
+        return f"Pendaftaran berhasil. Nomor antrian: {self.klinik_status[clinic_id]['current_queue']}"
 
-    def get_queue_status(self, clinic_name):
+    def get_queue_status(self, clinic_id):
         # Return the current queue status
         return {
-            "current_queue": self.klinik_status[clinic_name]["current_queue"],
-            "estimated_wait_time": self.klinik_status[clinic_name]["estimated_wait_time"],
+            "current_queue": self.klinik_status[clinic_id]["current_queue"],
+            "estimated_wait_time": self.klinik_status[clinic_id]["estimated_wait_time"],
         }
+    
+    def get_menu(self):
+        menu_text = "Selamat Datang di Antredis\n"
+        # open_klinik_count = 0
+        for id in self.klinik_status.keys():
+            if (self.klinik_status[id]["open"]):
+                # open_klinik_count += 1
+                menu_text += f"{id}. {self.klinik_status[id]['name']}\n"
+        
+        return menu_text
+
 
 def run_server():
     server = SimpleXMLRPCServer(("0.0.0.0", 8000))  # Listen on all available interfaces
     clinic_server = ClinicServer()
     server.register_instance(clinic_server)
     print("Server listening on port 8000...")
+
+    # thread_dequeue = threading.Thread(dequeue, args=(clinic_server,))
+    # thread_dequeue.start()
     server.serve_forever()
+    # thread_dequeue.join()
 
 if __name__ == "__main__":
     run_server()
